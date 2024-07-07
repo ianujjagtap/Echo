cefe1a88f36727fc7557c3e8d2367455d3131b59
Anuj2004-Jagtap <147546472+Anuj2004-Jagtap@users.noreply.github.com>
2024-07-07 15:45:34 +0530

    Developer Card

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,29 +1,39 @@
-import React, { useState , useEffect } from 'react';
-import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
-import Photo from "../images/myphoto.jpg"
+import React, { useState, useEffect } from 'react';
+import { FaTwitter, FaLinkedin, FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
+import ProductCard from "./productCard";
+import DeveloperCard from "./developer";
 
 
 const navbar = () => {
     const [showDeveloperCard, setShowDeveloperCard] = useState(false);
-    const [showProductCard, setProductCard] = useState(false);
+    const [showProductCard, setShowProductCard] = useState(false);
 
     const toggleDeveloperCard = () => {
         setShowDeveloperCard(!showDeveloperCard);
     };
     const toggleProductCard = () => {
-        setShowDeveloperCard(showDeveloperCard);
+        setShowProductCard(!showProductCard);
     };
 
     useEffect(() => {
         if (showDeveloperCard) {
-          const timer = setTimeout(() => {
-            setShowDeveloperCard(false);
-          }, 5000); // Adjust the timeout duration as needed (5000ms = 5s)
-    
-          return () => clearTimeout(timer); // Cleanup the timer on component unmount or if showDeveloperCard changes
+            const timer = setTimeout(() => {
+                setShowDeveloperCard(false);
+            }, 5000); // Adjust the timeout duration as needed (5000ms = 5s)
+
+            return () => clearTimeout(timer); // Cleanup the timer on component unmount or if showDeveloperCard changes
         }
-      }, [showDeveloperCard]);
-    
+    }, [showDeveloperCard]);
+    // useEffect(() => {
+    //     if (showProductCard) {
+    //         const timer = setTimeout(() => {
+    //             setShowProductCard(false);
+    //         }, 5000); // Adjust the timeout duration as needed (5000ms = 5s)
+
+    //         return () => clearTimeout(timer); // Cleanup the timer on component unmount or if showDeveloperCard changes
+    //     }
+    // }, [showProductCard]);
+
 
 
     return (
@@ -35,25 +45,16 @@ const navbar = () => {
                         <span className="text-white text-xl font-semibold max-md:text-2xl hover:text-teal-300 hover:scale-150 trasition duration-500 ease-in-out select-none">Echo</span>
                     </div>
                     <div className="flex space-x-4 max-md:text-sm">
-                        <a href="#" className="text-white hover:text-teal-300 max-md:hidden">Other Product</a>
-                        <a href="#" onClick={toggleDeveloperCard}  className="  text-white hover:text-teal-300 max-md:pt-2 ">About Developer</a>
+                        <a href="#" onClick={toggleProductCard} className="text-white hover:text-teal-300 max-md:hidden">Other Product</a>
+                        <a href="#" onClick={toggleDeveloperCard} className="  text-white hover:text-teal-300 max-md:pt-2 ">About Developer</a>
                     </div>
                 </div>
 
                 {showDeveloperCard && (
-                    <div className="developer-card p-6  absolute right-20  rounded-lg shadow-lg w-80 mt-8 max-md:right-8">
-                        <div className="flex flex-col items-center">
-                        <i  onClick={()=>setShowDeveloperCard(!showDeveloperCard)} className="ri-close-large-line absolute text-text-slate-400 hover:text-teal-300 right-8"></i>
-                            <img src={Photo} alt="Developer" className="w-32 h-32 rounded-full object-cover mb-4" />
-                            <h2 className="text-xl font-semibold mb-2">Jagtap Anuj</h2>
-                            <p className="text-gray-600 mb-4 text-center">Aspiring MERN Stack Developer Who Loves Crafting Web Apps❤️</p>
-                            <div className="flex space-x-4">
-                                <a href="https://twitter.com/JagtapAnuj15836" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700"><FaTwitter size={24} /></a>
-                                <a href="linkedin.com/in/anuj-jagtap-66a23429a" target="_blank" rel="noopener noreferrer" className="text-blue-700 hover:text-blue-900"><FaLinkedin size={24} /></a>
-                                <a href="https://github.com/Anuj2004-Jagtap" target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-gray-600"><FaGithub size={24} /></a>
-                            </div>
-                        </div>
-                    </div>
+                   <DeveloperCard setShowDeveloperCard={setShowDeveloperCard} showDeveloperCard={showDeveloperCard}/>
+                )}
+                {showProductCard && (
+                    <ProductCard setShowProductCard={setShowProductCard} showProductCard={showProductCard} />
                 )}
             </nav>
         </>
---
+++ src/components/productCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Project1 from "../images/copy-document.png";
+
+
+const developerCard = ({ setShowProductCard, showProductCard }) => {
+    return (
+        <>
+            <div className="developer-card p-6  absolute right-20 bg-slate-800 rounded-lg shadow-lg w-80 mt-8 max-md:right-8">
+                <div className="flex flex-col items-center">
+                    <i onClick={() => setShowProductCard(!showProductCard)} className="ri-close-large-line top-0 bg-slate-800 absolute text-text-slate-400 hover:text-teal-300 right-6"></i>
+
+                    <div className="product1 flex justify-center items-center w-[78%]  h-12 mt-2 pb-2">
+                        <img src={Project1} alt="" className='w-8 h-8' />
+                        <div className='pl-4 '>
+                            <div className='flex items-center'>Text-Klean <span className='pl-2 text-lg '><a href="https://text-klean.vercel.app/" target='_blank'><i className="ri-external-link-line text-teal-200"></i></a></span></div>
+                            <div className='text-xs text-slate-300 '>A Text File Cleaning App</div>
+                            <span></span>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default developerCard
